bd5d49cb3f6603e6184aee99f56c63641833bf0b
maksimzhohov <zhohovmaksim@gmail.com>
2024-10-11 00:07:45 +0300

    feat: add model for user log

--- backend/src/apps/logs/models.py
+++ backend/src/apps/logs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class UserLog(models.Model):
+    telegram_user_id = models.IntegerField(verbose_name="Telegram User ID")
+    user_command = models.CharField(verbose_name="User command", max_length=255)
+    bot_response = models.TextField(verbose_name="Response from bot")
+    timestamp = models.DateTimeField(verbose_name="Time", auto_now_add=True)
+
+    class Meta:
+        ordering = ['-timestamp']
+        indexes = [
+            models.Index(fields=['telegram_user_id'])
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.telegram_user_id}, {self.user_command}, {self.timestamp}"
+
